ef7a27cc21e21d50af9d0d16ffccbf10a77f58f5
Almas Serik <s.almas_99@mail.ru>
2023-01-12 19:12:33 +0600

    reading port number from config file

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,25 @@ import (
 	"github.com/HgCl2/rock_scissors_paper/pkg/handler"
 	"github.com/HgCl2/rock_scissors_paper/pkg/repository"
 	"github.com/HgCl2/rock_scissors_paper/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(app.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error run http server %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
