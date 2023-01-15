70bb76aec308f514de465f5ed5772fdaa84a03b9
Almas Serik <s.almas_99@mail.ru>
2023-01-15 20:56:00 +0600

    generatePasswordHash writed

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -1,9 +1,14 @@
 package service
 
 import (
+	"crypto/sha1"
+	"fmt"
+
 	app "github.com/HgCl2/rock_scissors_paper"
 	"github.com/HgCl2/rock_scissors_paper/pkg/repository"
 )
+
+const salt = "KazakhstanForever"
 
 type AuthService struct {
 	repo repository.Authorization
@@ -14,5 +19,13 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 }
 
 func (s *AuthService) CreateUser(user app.Player) (int, error) {
+	user.Password = s.generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
 }
