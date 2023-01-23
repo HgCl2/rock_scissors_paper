dd3543c6b76eb6bc2f9e599afede7401925fe680
Almas Serik <s.almas_99@mail.ru>
2023-01-23 17:36:12 +0600

    handlers for authentication using google

--- pkg/handler/response.go
+++ pkg/handler/response.go
@@ -6,11 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type error struct {
+type Error struct {
 	Message string `json:"message"`
 }
 
 func newErrorResponse(c *gin.Context, statusCode int, message string) {
 	log.Fatalf("%s\n", message)
-	c.AbortWithStatusJSON(statusCode, error{Message: message})
+	c.AbortWithStatusJSON(statusCode, Error{Message: message})
 }
